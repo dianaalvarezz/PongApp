e833699f5669d7b75ed4955607f8d14e6cb6e5b7
Diana Alvarez <143985084+dianaalvarezz@users.noreply.github.com>
2025-02-06 22:49:20 -0600

    Update BallModel.java

--- BallModel.java
+++ BallModel.java
@@ -13,6 +13,7 @@ public class BallModel
     private int paddleWidth;
     private int paddleHeight;
     private int ballVelocity;
+    private int ballVelocityX;
 
     public BallModel(int screenWidth, int screenHeight)
     {
@@ -24,6 +25,8 @@ public class BallModel
         ballY = screenHeight / 2;
         ballX = screenWidth / 2;
         ballVelocity = 5;
+        ballVelocityX = 5;
+
     }
 
     public Point getPaddleCenter()
@@ -46,17 +49,42 @@ public class BallModel
         paddleX = Math.max(0, Math.min(newX, screenWidth - paddleWidth));
     }
 
-    public void incrementBallY(int screenHeight)
+    public void incrementBallY(int screenHeight, int screenWidth)
     {
         ballY += ballVelocity;
+        ballX += ballVelocityX;
+
 
         // Reverse direction if the ball hits the top or bottom
         if (ballY <= 0 || ballY >= screenHeight - 30)
         {
             ballVelocity = -ballVelocity;
         }
+
+        if (ballY + 30 >= paddleY && ballX >= paddleX && ballX <= paddleX + paddleWidth)
+        {
+            ballVelocity = -ballVelocity;
+        }
+
+        if (ballY > screenHeight)
+        {
+            resetBall(screenHeight, screenWidth);
+        }
+
+        // Reverse direction if the ball hits the left or right wall
+        if (ballX <= 0 || ballX >= screenWidth - 30)
+        {
+            ballVelocityX = -ballVelocityX;
+        }
+
     }
 
+
+    public void resetBall(int screenHeight, int screenWidth)
+    {
+        ballY = screenHeight / 2;
+        ballX = screenWidth / 2;
+    }
 
     public boolean inBounds(int screenWidth, int newX)
     {
